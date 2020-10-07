c3ea3e559ce97dae135b76d185d2ab42dc74111d
apd4499 <alifpd4499@gmail.com>
2020-10-07 12:37:44 +0700

    update get

---
+++ pertemuan4/video/GET.php
@@ -0,0 +1,14 @@
+<?php
+//------------------------
+//------GET Dan POST------
+//------------------------
+
+echo $_GET['password']
+
+?>
+
+<form action="GET.php" method="get">
+    <input type="text" name="nama">
+    <input type="password" name="password">
+    <input type="submit" name="submit">
+</form>

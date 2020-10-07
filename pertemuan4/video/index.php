76f55b90fca6616bb7d487f07480962d3d39a438
apd4499 <alifpd4499@gmail.com>
2020-10-07 12:42:47 +0700

    update

---
+++ pertemuan4/video/index.php
@@ -0,0 +1,14 @@
+<?php
+//------------------------
+//------GET Dan POST------
+//------------------------
+
+echo $_GET['password']
+
+?>
+
+<form action="index.php" method="get">
+    <input type="text" name="nama">
+    <input type="password" name="password">
+    <input type="submit" name="submit">
+</form>
